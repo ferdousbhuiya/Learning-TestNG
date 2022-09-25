2c8b7ea66baedb23f50c4a6a9412b6087b48547d
ferdousbhuiya <ferdousbhuiya@gmail.com>
2022-09-24 23:49:21 -0400

    This is first commit.

---
+++ src/LearningTestNG/TestingTestNG.java
@@ -0,0 +1,13 @@
+package LearningTestNG;
+import org.testng.annotations.Test;
+
+
+public class TestingTestNG {
+
+	@Test
+	
+	public void testcase1 () {
+		System.out.println("First Test Case");
+	}
+
+}
